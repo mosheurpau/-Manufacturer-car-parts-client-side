247ea8e86648eb8fe9fcb230dbd41ffa2db64ea8
mosheurpau <mosheur.pau@gamil.com>
2022-05-23 01:57:15 +0600

    added banner

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -2,8 +2,27 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div>
-      <h2>This is banner</h2>
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: "url(https://i.ibb.co/fk4xzYF/banner-bg02.jpg)",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-0"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="me-">
+          <h1 className="mb-10 text-4xl md:text-7xl text-white font-bold">
+            FIND PERFECT CAR PARTS
+          </h1>
+          <p className="mb-10 text-2xl text-white font-bold">
+            A car parts dealership, or vehicle local distribution, is a
+            business.
+          </p>
+          <button className="btn btn-primary font-bold px-5 uppercase">
+            Find it now
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Banner from "./Banner";
 
 const Home = () => {
   return (
     <div>
-      <h2>This is Banner</h2>
+      <Banner></Banner>
     </div>
   );
 };
